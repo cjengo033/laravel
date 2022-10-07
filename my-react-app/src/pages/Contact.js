93d60153ce01ce7a4c590ac4677b17a12442464d
cjengo033 <201810094@fit.edu.ph>
2022-10-07 19:20:08 +0800

    contact page fe

--- my-react-app/src/pages/Contact.js
+++ my-react-app/src/pages/Contact.js
@@ -1,5 +1,64 @@
+import React from 'react'
+import message from '../assets/message.png';
+
 const Contact = () => {
-    return <h1>Contact Me</h1>;
-  };
-  
-  export default Contact;
+  return (
+    <>
+      <div className='text-center mt-4'>
+        <h1>HAVE SOME QUESTIONS?</h1>
+        <p><a href="https://github.com/cjengo033">Github Profile</a></p>
+      </div>
+      <div className='shadow p-3 mb-5 bg-body rounded bg-body rounded d-flex flex-row m-5 align-items-center justify-content-center'>
+        
+        <div className='mb-5 mt-5 mr-5'>
+          <img src={message} alt="first-image.jpg" height={"500dp"} width={"500dp"}/>
+        </div>
+
+        <div className=' ml-5 mr-4'>
+          <form>
+            <div className='form-group'>
+              <label>First name</label>
+              <input type="text" 
+              class="form-control" 
+              placeholder='First name'
+              aria-label="Sizing example input" 
+              aria-describedby="inputGroup-sizing-lg" />
+            </div>
+
+            <div className='form-group'>
+              <label>Last name</label>
+              <input type="text" 
+              placeholder='Last name'
+              class="form-control" 
+              aria-label="Sizing example input" 
+              aria-describedby="inputGroup-sizing-lg" />
+            </div>
+
+            <div className='form-group'>
+              <label>What's your email?</label>
+              <input type="text" 
+              placeholder='Email'
+              class="form-control" 
+              aria-label="Sizing example input" 
+              aria-describedby="inputGroup-sizing-lg" />
+            </div>
+
+
+            <div className='form-group'>
+              <label>Concern:</label>
+              <textarea rows="4" cols="50" name="comment" form="usrform" className='form-control form-control-sm' placeholder='Message...'/>
+            </div>
+            <button className='btn btn-dark mt-4 p-3'>SEND MESSAGE</button>
+
+          </form>
+
+        </div>
+
+
+      </div>
+
+    </>
+  )
+}
+
+export default Contact
--- my-react-app/src/pages/Layout.js
+++ my-react-app/src/pages/Layout.js
@@ -5,7 +5,6 @@ const Layout = () => {
     <>
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
                 <div class="container-fluid">
-                    <a className="navbar-brand" href="/">Home</a>
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
                     </button>
