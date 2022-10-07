4dc4b784b200516d8c47c2300988e83c3ce6e2f7
cjengo033 <201810094@fit.edu.ph>
2022-10-07 12:47:40 +0800

    Router
    settings up router and pages

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,14 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from '././component/Header';
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./pages/Layout";
+import Home from "./pages/Home";
+import Blogs from "./pages/Blogs";
+import Contact from "./pages/Contact";
+import NoPage from "./pages/NoPage";
 
 function App() {
   return (
     <>
-      <div className='bg-primary text-center'>
-         <Header />
-      </div>
-    
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="blogs" element={<Blogs />} />
+          <Route path="contact" element={<Contact />} />
+          <Route path="*" element={<NoPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter> 
     </>
   );
 }
