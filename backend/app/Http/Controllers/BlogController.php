df86b37ebbfe692dd412c7a032e42d1eb1050836
cjengo033 <201810094@fit.edu.ph>
2022-10-11 17:37:02 +0800

    indent fix

--- backend/app/Http/Controllers/BlogController.php
+++ backend/app/Http/Controllers/BlogController.php
@@ -84,19 +84,20 @@ class BlogController extends Controller
 
     public function showMe(Request $request)
     {
-       $user_data =  User::where('name','like','%'.$request->name.'%')->get();
-       if($user_data){
-        return $user_data;
-       }else {
-        return "No Data has been found";
-       }
+        $user_data =  User::where('name', 'like', '%' . $request->name . '%')->get();
+        if ($user_data) {
+            return $user_data;
+        } else {
+            return "No Data has been found";
+        }
     }
 
-    public function register(Request $request) {
+    public function register(Request $request)
+    {
         $user_data = $request->validate([
             "name" => ['required', 'min:4'],
             "password" => ['required', 'min:4'],
-            "email" => ['required', 'email'] 
+            "email" => ['required', 'email']
         ]);
 
         $user = User::create([
@@ -104,7 +105,7 @@ class BlogController extends Controller
             'email' => $request->email,
             'password' => Hash::make($request->password)
         ]);
-        
+
         $token = $user->createToken('myapptoken')->plainTextToken;
 
         $response = [
@@ -113,51 +114,56 @@ class BlogController extends Controller
         ];
 
         return response($response, 201);
-     }
+    }
 
-     public function testToken(){
+    public function testToken()
+    {
         return "Working api token";
-     }
+    }
 
-     public function logout(Request $request){
+    public function logout(Request $request)
+    {
 
         $request->user()->currentAccessToken()->delete();
 
         return [
-            'message'=>'Logged out'
+            'message' => 'Logged out'
         ];
     }
 
-    public function login(Request $request){
+    public function login(Request $request)
+    {
 
-            $validateUser = Validator::make($request->all(), 
+        $validateUser = Validator::make(
+            $request->all(),
             [
                 'email' => 'required|email',
                 'password' => 'required'
-            ]);
+            ]
+        );
 
-            if($validateUser->fails()){
-                return response()->json([
-                    'status' => false,
-                    'message' => 'validation error',
-                    'errors' => $validateUser->errors()
-                ], 401);
-            }
-
-            if(!Auth::attempt($request->only(['email', 'password']))){
-                return response()->json([
-                    'status' => false,
-                    'message' => 'Email & Password does not match with our record.',
-                ], 401);
-            }
-
-            $user = User::where('email', $request->email)->first();
-
+        if ($validateUser->fails()) {
             return response()->json([
-                'status' => true,
-                'message' => 'User Logged In Successfully',
-                'token' => $user->createToken("API TOKEN")->plainTextToken
-            ], 200);
+                'status' => false,
+                'message' => 'validation error',
+                'errors' => $validateUser->errors()
+            ], 401);
+        }
+
+        if (!Auth::attempt($request->only(['email', 'password']))) {
+            return response()->json([
+                'status' => false,
+                'message' => 'Email & Password does not match with our record.',
+            ], 401);
+        }
+
+        $user = User::where('email', $request->email)->first();
+
+        return response()->json([
+            'status' => true,
+            'message' => 'User Logged In Successfully',
+            'token' => $user->createToken("API TOKEN")->plainTextToken
+        ], 200);
 
         // if(auth()->attempt($validated)){
         //     return response()->json([
@@ -165,7 +171,7 @@ class BlogController extends Controller
         //         'message' => 'User Logged In Successfully',
         //         'token' => $user->createToken("API TOKEN")->plainTextToken
         //     ], 200);
-             
+
         // }else {
         //     return response()->json([
         //         'status' => false,
@@ -173,8 +179,5 @@ class BlogController extends Controller
         //     ], 401);
         // }
 
-        
-
-        
     }
 }
